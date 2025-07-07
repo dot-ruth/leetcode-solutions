75950106fb08c5b5cbf193cd30a4478d627bc95d
Ruth Abiti <ruthgetaneh5@gmail.com>
2025-07-07 22:00:16 +0300

    Time: 3 ms (46.25%) | Memory: 12.7 MB (48.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -4,25 +4,61 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        # ( -> (, ) -> ((),()
         stack = []
         res = []
 
         def backtrack(open, close):
-            if open == close == n:
-                res.append("".join(stack))
-                return 
-
             if open < n:
                 stack.append("(")
-                backtrack(open +1, close)
+                backtrack(open + 1, close)
                 stack.pop()
 
-            if close < open: 
+            if close < open:
                 stack.append(")")
                 backtrack(open, close + 1)
                 stack.pop()
 
+            if open == close == n: 
+                res.append("".join(stack))
+
         backtrack(0,0)
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+        # res = []
+
+        # def backtrack(open, close):
+        #     if open == close == n:
+        #         res.append("".join(stack))
+        #         return 
+
+        #     if open < n:
+        #         stack.append("(")
+        #         backtrack(open +1, close)
+        #         stack.pop()
+
+        #     if close < open: 
+        #         stack.append(")")
+        #         backtrack(open, close + 1)
+        #         stack.pop()
+
+        # backtrack(0,0)
+        # return res
 
         
